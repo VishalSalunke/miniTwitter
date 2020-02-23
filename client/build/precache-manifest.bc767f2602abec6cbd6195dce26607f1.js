e80e7ac665aa81525a957c011a319c65fe9ce10a
vishal <salunkevishal05@gmail.com>
2020-02-23 18:33:57 +0530

    gitignore issue

---
+++ client/build/precache-manifest.bc767f2602abec6cbd6195dce26607f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d2b67d6767238da1bfbf2c59a4b239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b95a2bae2bf4c36f85f",
+    "url": "/static/css/main.5e7f6a52.chunk.css"
+  },
+  {
+    "revision": "7cbcb2362d9408e8d338",
+    "url": "/static/js/2.69e50153.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.69e50153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b95a2bae2bf4c36f85f",
+    "url": "/static/js/main.a5ce0e40.chunk.js"
+  },
+  {
+    "revision": "289ea4f5dbf0ed9f540a",
+    "url": "/static/js/runtime-main.8ba21d55.js"
+  }
+]);
